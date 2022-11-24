aaf56ae218e8c227de3c830b02153b830c5a6872
Jawad Dhew <jawaddhew98@gmail.com>
2022-11-24 22:51:19 +0600

    adding done

--- src/Pages/DashBoard/MyOrders/Seller/Addproducts.js
+++ src/Pages/DashBoard/MyOrders/Seller/Addproducts.js
@@ -4,9 +4,51 @@ import Form from 'react-bootstrap/Form';
 
 const Addproducts = () => {
 
+    var cat_id;
     const { user } = useContext(AuthContext);
-    const handleProducts = () => {
+    const handleProducts = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = user?.displayName;
+        const email = user?.email || 'unregistered';
+        const img = form.image.value;
+        const Orgprice = form.Orgprice.value;
+        const price = form.price.value;
+        const use = form.used.value;
+        const location = form.location.value;
+        const brand = form.brand.value;
 
+
+        if (brand === "asus") {
+            cat_id = "637ee62bf3ee5834ef8fa06c";
+        }
+
+        const review = {
+            services: _id,
+            serviceName: item,
+            customer: name,
+            rating: rate,
+            email,
+            message,
+            img
+        }
+        fetch('https://server-six-kappa.vercel.app/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.acknowledged) {
+                    alert('Thanks for Sharing Your Views!')
+                    form.reset();
+
+                }
+            })
+            .catch(er => console.error(er));
     }
 
     return (
@@ -21,6 +63,10 @@ const Addproducts = () => {
                     <Form.Label>Email address</Form.Label>
                     <Form.Control defaultValue={user?.email} type="email" placeholder="name@example.com" />
                 </Form.Group>
+                <Form.Group className="mb-3" controlId="item">
+                    <Form.Label for="item">Item Name</Form.Label>
+                    <Form.Control id="item" type="text" placeholder="Enter Item Name " />
+                </Form.Group>
                 <Form.Group className="mb-3" controlId="Orgprice">
                     <Form.Label for="Orgprice">Original Price</Form.Label>
                     <Form.Control id="Orgprice" type="text" placeholder="Enter Price" />
@@ -29,9 +75,9 @@ const Addproducts = () => {
                     <Form.Label for="price" >Resale Price</Form.Label>
                     <Form.Control id="price" type="text" placeholder="Enter Price" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="number">
-                    <Form.Label for="number" >Phone Number</Form.Label>
-                    <Form.Control id="number" type="text" placeholder="Enter your number" />
+                <Form.Group className="mb-3" controlId="image">
+                    <Form.Label for="image" >Image url</Form.Label>
+                    <Form.Control id="image" type="text" placeholder="Enter image url" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="location">
                     <Form.Label for="location">Location</Form.Label>
